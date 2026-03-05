7f6d168c01ebd8118329686afbe2516c9d0d88b4
julia123345 <julianovikova2005@yandex.ru>
2026-03-05 15:39:49 +0300

    Delete programming/LabAcadem2sem directory

--- programming/LabAcadem2sem/common/src/main/java/app/ResponseStatus.java
+++
@@ -1,7 +0,0 @@
-package app;
-
-import java.io.Serializable;
-
-public enum ResponseStatus implements Serializable {
-    OK, ERROR, DENIED, AUTH_FAILED, AUTH_PASSED
-}
--- programming/LabAcadem2sem/common/src/main/java/app/model/Country.java
+++
@@ -1,9 +0,0 @@
-package app.model;
-
-import java.io.Serializable;
-
-public enum Country implements Serializable {
-    FRANCE,
-    VATICAN,
-    JAPAN
-}
